f2e9eae693010853ef5e0b1d0bad9bf10d8a0653
ayush.singh <ayush.singh@ivycomptech.com>
2024-10-21 22:01:38 +0530

    Initial commit

---
+++ src/main/java/com/lcwd/gateway/models/AuthResponse.java
@@ -0,0 +1,78 @@
+package com.lcwd.gateway.models;
+
+import java.util.Collection;
+
+public class AuthResponse {
+
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+    private long expireAt;
+    private Collection<String> authorities;
+
+    // No-argument constructor
+    public AuthResponse() {
+    }
+
+    // All-argument constructor
+    public AuthResponse(String userId, String accessToken, String refreshToken, long expireAt, Collection<String> authorities) {
+        this.userId = userId;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.expireAt = expireAt;
+        this.authorities = authorities;
+    }
+
+    // Getters and Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public long getExpireAt() {
+        return expireAt;
+    }
+
+    public void setExpireAt(long expireAt) {
+        this.expireAt = expireAt;
+    }
+
+    public Collection<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<String> authorities) {
+        this.authorities = authorities;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "AuthResponse{" +
+                "userId='" + userId + '\'' +
+                ", accessToken='" + accessToken + '\'' +
+                ", refreshToken='" + refreshToken + '\'' +
+                ", expireAt=" + expireAt +
+                ", authorities=" + authorities +
+                '}';
+    }
+}
